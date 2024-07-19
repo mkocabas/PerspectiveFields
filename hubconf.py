1fa38e8725a5dad4baabfd4dd27bfd96ddee6881
mkocabas <muhammed@meshcapade.com>
2024-07-19 20:32:29 +0000

    add hubconf

--- hubconf.py
+++ hubconf.py
@@ -17,3 +17,14 @@ def perspective_fields(pretrain=True, **kwargs):
     version = kwargs.get('version', 'Paramnet-360Cities-edina-centered')
     pf_model = PerspectiveFields(version).eval().cuda()
     return pf_model
+
+
+'''Usage:
+import cv2
+import torch
+model = torch.hub.load('mkocabas/PerspectiveFields', "perspective_fields", pretrained=True)
+
+bgr = cv2.imread(img_path)
+pred = model.inference(bgr)
+print(f"roll: {pred['pred_roll'].item()}\npitch: {pred['pred_pitch'].item()}\nvfov: {pred['pred_vfov'].item()}\ncx: {pred['pred_rel_cx'].item()}\ncy: {pred['pred_rel_cy'].item()}")
+'''
